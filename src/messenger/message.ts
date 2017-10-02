fe16e09b9a451ec01a60a0b9e7251d265f5ad936
Giuseppe Riolo <riolo@innovactive.it>
2017-10-02 23:40:40 +0200

    renaming in javascript conventions

--- src/messenger/message.ts
+++ src/messenger/message.ts
@@ -1,17 +1,18 @@
 import Serializable from './../serializer/serializable'
 
 abstract class Message  {
-    public abstract Start:number
-    public abstract Head: number[]
-    public abstract End?:number
+    public abstract start:number
+    public abstract head: number[]
+    public abstract end?:number
+    public length: number;
 
     public toBytes(Data : Serializable){
         let arr = new Array<number>();
-        arr.push(this.Start)
-        arr.push(... this.Head);
+        arr.push(this.start)
+        arr.push(... this.head);
         arr.push(... (<Array<number>><any>Data.serialize()));
-        if(typeof(this.End)==="number")
-            arr.push(this.End);
+        if(typeof(this.end)==="number")
+            arr.push(this.end);
         return Buffer.from(arr);
     }
 
