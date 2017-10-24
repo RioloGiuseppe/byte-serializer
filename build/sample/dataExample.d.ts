216859dbbfdd6158f6fbad3e578bdc8f1f31e8d8
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2017-10-24 17:10:56 +0200

    rem build

--- build/sample/dataExample.d.ts
+++
@@ -1,9 +0,0 @@
-import { Serializable } from './../serializer/serializable';
-/**
- * @ignore
- */
-export declare class DataExample extends Serializable {
-    Pippo: number;
-    Pluto: number;
-    Text: string;
-}
